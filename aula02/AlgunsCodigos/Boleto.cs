83d85016975f546a9389bd75c73ed07e73c11133
otav-o <otaviocacadordioscanio@gmail.com>
2021-02-22 19:24:51 -0300

    Adicionar classe Boleto e testar função Lambda

---
+++ aula02/AlgunsCodigos/Boleto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgunsCodigos
+{
+    class Boleto
+    {
+        private DateTime data;
+        private double valor;
+
+        public DateTime Data { get => data; set => data = value; }
+        // public DateTime Data { get { return data; } set { data = value; } }
+        // operador lambda: executa uma expressão. Se for apenas uma variável, entende como retorno
+
+        public double Valor { get => valor; set => valor = value; }
+    }
+}
