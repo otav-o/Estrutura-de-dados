b063829adbad078777ce346cc310173da75bf2ad
otav-o <otaviocacadordioscanio@gmail.com>
2021-02-22 19:35:01 -0300

    Reorganização da classe Boleto e inserção do método EstaVencido

--- aula02/AlgunsCodigos/Boleto.cs
+++ aula02/AlgunsCodigos/Boleto.cs
@@ -6,13 +6,15 @@ namespace AlgunsCodigos
 {
     class Boleto
     {
-        private DateTime data;
-        private double valor;
-
-        public DateTime Data { get => data; set => data = value; }
+        public DateTime DataVencimento { get => dataVencimento; set => dataVencimento = value; }
         // public DateTime Data { get { return data; } set { data = value; } }
         // operador lambda: executa uma expressão. Se for apenas uma variável, entende como retorno
 
         public double Valor { get => valor; set => valor = value; }
+
+        public bool EstaVencido() => DateTime.Now.Date > DataVencimento.Date;
+
+        private DateTime dataVencimento;
+        private double valor;
     }
 }
