f7632814a3a91d44363e4bf41f863332c52baa9b
otav-o <otaviocacadordioscanio@gmail.com>
2021-02-22 19:55:19 -0300

    Criação de método com parâmetros do tipo object

--- aula02/AlgunsCodigos/Program.cs
+++ aula02/AlgunsCodigos/Program.cs
@@ -6,7 +6,25 @@ namespace AlgunsCodigos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var boletos = new Boleto[]
+            {
+                new Boleto { DataVencimento = new DateTime(2021, 3, 10), Valor = 120 },
+                new Boleto { DataVencimento = new DateTime(2021, 4, 10), Valor = 115 },
+                new Boleto { DataVencimento = new DateTime(2021, 5, 10), Valor = 110 }
+            };
+
+            for (int i = 0; i < boletos.Length; i++)
+            {
+                WriteLine("{0} - {1}", boletos[i].DataVencimento, boletos[i].Valor);
+            }
         }
+
+        static void WriteLine(string formato, object data, object valor)
+        {
+            string novaString = formato.Replace("{0}", data.ToString());
+            novaString = novaString.Replace("{1}", valor.ToString());
+            Console.WriteLine(novaString);
+        }
+        // todo objeto em C# herda de Object, e este tem um método ToString()
     }
 }
