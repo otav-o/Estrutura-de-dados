770dcac50dbb9f923581e7e8d972db04b5e674c9
otav-o <otaviocacadordioscanio@gmail.com>
2021-03-06 16:01:11 -0300

    Criação de um projeto só para várias aulas

---
+++ EstruturaDeDados/EstruturaDeDados/Program.cs
@@ -0,0 +1,27 @@
+﻿using EstruturaDeDados.Recursividade;
+using System;
+using System.Collections;
+
+namespace EstruturaDeDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 55, 23, 12, 1, 5, 2, 3, 2, 85, 23, 0, 04 };
+            var teste = new SemRecursividade();
+            teste.OrdenarVetor(array);
+
+            ImprimirColecao(array);
+
+        }
+
+        private static void ImprimirColecao(IEnumerable vetor)
+        {
+            foreach(var elemento in vetor)
+            {
+                Console.WriteLine(elemento);
+            }
+        }
+    }
+}
---
+++ EstruturaDeDados/EstruturaDeDados/Recursividade/SemRecursividade.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EstruturaDeDados.Recursividade
+{
+    class SemRecursividade
+    {
+        public void OrdenarVetor(int[] A)
+        {
+            int i, j, k, t;
+            for (i = 0; i < (A.Length - 1); i++)
+            {
+                j = i;
+                for (k = (i + 1); k < A.Length; k++)
+                    if (A[k] < A[i])
+                        j = k;
+                t = A[i];
+                A[i] = A[j];
+                A[j] = t;
+            }
+        }
+    }
+}
