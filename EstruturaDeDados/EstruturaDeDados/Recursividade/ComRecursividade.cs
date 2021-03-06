1b894e658cf814004283557c30f69813aa1b10f0
otav-o <otaviocacadordioscanio@gmail.com>
2021-03-06 16:04:09 -0300

    Transformação de for em while

--- EstruturaDeDados/EstruturaDeDados/Recursividade/ComRecursividade.cs
+++ EstruturaDeDados/EstruturaDeDados/Recursividade/ComRecursividade.cs
@@ -6,5 +6,23 @@ namespace EstruturaDeDados.Recursividade
 {
     class ComRecursividade
     {
+        public void OrdenarVetor(int[] A)
+        {
+            int i, j, k, t;
+            for (i = 0; i < (A.Length - 1); i++)
+            {
+                j = i;
+                k = (i + 1);
+                while (k < A.Length) // etapa 1: transformar o for em while
+                {
+                    if (A[k] < A[i])
+                        j = k;
+                    k++;
+                }
+                t = A[i];
+                A[i] = A[j];
+                A[j] = t;
+            }
+        }
     }
 }
