e9fcf6c53fbce66317e11fcd491e17b3bdb7b37e
otav-o <otaviocacadordioscanio@gmail.com>
2021-03-11 19:33:27 -0300

    Testando o ref

---
+++ Fatorial/Fatorial/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Fatorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            Procedimento(ref x);
+            Console.WriteLine(x);
+        }
+
+        private static void Procedimento(ref int x)
+        {
+            x *= 2;
+        }
+    }
+}
