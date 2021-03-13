5e0499d77ca5e9f0785a2c7b5539f0bd053c5a01
otav-o <otaviocacadordioscanio@gmail.com>
2021-03-13 20:17:38 -0300

    Criação da classe FatorialPassagemPorRef para preservar o código

---
+++ Fatorial/FatorialPassagemPorRef.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fatorial
+{
+    class FatorialPassagemPorRef
+    {
+        public static void Executar()
+        {
+            Console.WriteLine("5! usando passagem por referência: " + Fatorial(5));
+        }
+
+        static int Fatorial(int numero)
+        {
+            int resultado = 1;
+
+            Fatorial(numero, ref resultado); // não passa uma cópia do valor, e sim o espaço de memória
+
+            return resultado;
+        }
+
+        static void Fatorial(int numero, ref int resultado)
+        {
+            if (numero > 1)
+            {
+                resultado *= numero;
+                Fatorial(numero - 1, ref resultado);
+            }
+        }
+    }
+}
--- Fatorial/Program.cs
+++ Fatorial/Program.cs
@@ -6,25 +6,7 @@ namespace Fatorial
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Fatorial(5));
-        }
-        // Fatorial com passagem por referência
-        static int Fatorial(int numero)
-        {
-            int resultado = 1;
-
-            Fatorial(numero, ref resultado); // não passa uma cópia do valor, e sim o espaço de memória
-
-            return resultado;
-        }
-
-        static void Fatorial(int numero, ref int resultado)
-        {
-            if (numero > 1)
-            {
-                resultado *= numero;
-                Fatorial(numero - 1, ref resultado);
-            }       
+            FatorialPassagemPorRef.Executar();
         }
     }
 }
