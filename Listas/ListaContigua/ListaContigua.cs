89fd5514d05a4b9f5547047285874c4605e84140
otav-o <otaviocacadordioscanio@gmail.com>
2021-04-19 22:57:01 -0300

    Aula 19-04-2021

---
+++ Listas/ListaContigua/ListaContigua.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListasContiguas
+{
+    class ListaContigua
+    {
+        public void Adicionar(double valor)
+        {
+            Exception erro = new Exception("Lista cheia");
+
+            if (tamanho >= TAMANHO_MAXIMO)
+                throw erro;
+
+            itens[tamanho] = valor;
+            tamanho++;
+        }
+
+        public void RemoverNoFinal()
+        {
+            if (tamanho <= 0)
+                throw new Exception("Não é possível remover um elemento de uma lista vazia.");
+
+            tamanho--;
+        }
+
+        public double RetornarItem(int indice)
+        {
+            if (indice < 0 || indice >= tamanho)
+                throw new ArgumentOutOfRangeException("O índice está fora dos limites da lista.");
+
+            return itens[indice];
+
+        }
+
+        public double this[int indice] // this + colchetes: indexador (somente C#)
+        {
+            get
+            {
+                if (indice < 0 || indice >= tamanho)
+                    throw new ArgumentOutOfRangeException("O índice está fora dos limites da lista.");
+
+                return itens[indice];
+            }
+            set
+            {
+                if (indice < 0 || indice >= tamanho)
+                    throw new ArgumentOutOfRangeException("O índice está fora dos limites da lista.");
+
+                itens[indice] = value;
+            }
+
+
+        }
+
+        public int Tamanho { get => tamanho; }
+
+        private int tamanho = 0;
+        private const int TAMANHO_MAXIMO = 100;
+        private double[] itens = new double[100];
+    }
+}
---
+++ Listas/ListaContigua/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ListasContiguas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lista = new ListaContigua();
+            lista.Adicionar(3.5);
+            lista.Adicionar(12.2);
+            lista.Adicionar(5);
+
+            try
+            {
+                lista[2] = 3;
+                lista[5] = 3;
+            } catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Imprimir(lista);           
+        }
+
+        static void Imprimir(ListaContigua lista)
+        {
+            for (int i = 0; i < lista.Tamanho; i++)
+            {
+                Console.WriteLine(lista.RetornarItem(i));
+            }
+        }
+    }
+}
